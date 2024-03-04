afb5ed69ed30df3b4a8db8c2f5701d621520194f
Victor-Estrella <161384189+Victor-Estrella@users.noreply.github.com>
2024-03-04 10:19:30 -0300

    Add files via upload

---
+++ exemplo1.py
@@ -0,0 +1,14 @@
+print('*-*Meu primeiro programa em Python*=*')
+
+#Entrada de Dados
+n1 = int(input('Número 1: '))
+n2 = int(input('Número 2: '))
+
+#Processamento
+sub = n1 - n2
+
+#Saída de dados
+print('A subtração de', n1, '-', n2, 'é', sub)
+print('Ótima semana!')
+print('Seu programa terminou!')
+print('Vai \n embora cara!')
---
+++ teste.py
@@ -0,0 +1,14 @@
+print('Coloque a nota')
+
+#Entrada
+n1 = int(input('Nota 1: '))
+n2 = int(input('Nota 2: '))
+n3 = int(input('Nota 3: '))
+n4 = int(input('Nota 4: '))
+
+#Processamento
+med= (n1+n2+n3+n4)/4
+
+#Saída
+print('A sua nota é', med)
+print('Bom final de ano')
